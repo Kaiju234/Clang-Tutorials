a74e26c7442926deabacd8b58f0f56ff92f1e6fb
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2023-12-10 12:26:06 -0600

    Add files via upload

---
+++ Indentifier.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+int main(){
+
+
+text_t firstName = "RajShah";
+
+std::cout << firstName << '\n';
+
+    return 0;
+}
