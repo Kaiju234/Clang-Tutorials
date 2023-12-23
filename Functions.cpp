6e99d02322e977d4ff4581dede18ea4fab896f91
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2023-12-22 19:16:12 -0600

    Add files via upload

---
+++ Functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Defining the Function \\
+
+void Banana();
+
+// Defining the main Method \\
+
+int main() {
+	Banana(); // Calling the Fruit
+
+	return 0;
+
+}
+
+// Function Defention 
+void Banana() {
+	cout << "help i turned into a bananasplit";
+}
+
